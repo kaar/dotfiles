743879c78428b8ac6462ddc6a111b1cf94ac8b5e
Caspar Nettelbladt <caspar.n@gmail.com>
2021-01-13 13:43:21 +0100

    apt upgrade before install packages

--- .scripts/install-packages.sh
+++ .scripts/install-packages.sh
@@ -1,3 +1,4 @@
 # Install all packates in ~/.packages
 sudo apt update
-sudo apt-get install $(awk -f ~/.awk/skip_hash.awk ~/.packages)
+sudo apt upgrade
+sudo apt install $(awk -f ~/.awk/skip_hash.awk ~/.packages)
