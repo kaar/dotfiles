5c561d3c2fa52ad71a40659b6369ee1b5f1bd15b
Caspar Nettelbladt <caspar.n@gmail.com>
2021-01-16 18:07:33 +0100

    Install Tmux Plugin Manager

--- .scripts/install-packages.sh
+++ .scripts/install-packages.sh
@@ -1,3 +1,8 @@
+# Load colors
+function header() {
+	echo -e "\033[0;33m$1\033[0m"
+}
+
 # Install all packates in ~/.packages
 sudo apt update
 sudo apt upgrade
@@ -8,6 +13,18 @@ BIN=~/.bin
 mkdir -p $BIN
 
 ## cheat.sh, https://github.com/chubin/cheat.sh
+header "Cheat.sh"
 curl --silent https://cht.sh/:cht.sh > $BIN/cht.sh
 chmod +x $BIN/cht.sh
 curl --silent https://cheat.sh/:bash_completion > ~/.config/cht.sh/completion.bash
+
+## Tmux Plugin Manager, https://github.com/tmux-plugins/tpm
+header "Tmux Plugin Manager"
+TMUX_PLUGINS=~/.config/tmux/plugins/tpm
+mkdir -p $TMUX_PLUGINS
+if [[ -d "$TMUX_PLUGINS/.git" ]]
+then
+	git -C $TMUX_PLUGINS pull
+else
+	git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGINS"
+fi
