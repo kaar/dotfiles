5a828b44fa182560528470e6cb84d06d63ee3f11
Caspar Nettelbladt <caspar.n@gmail.com>
2021-01-17 20:47:28 +0100

    minor prompt fixes

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,6 +1,4 @@
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
-Blue="\[\033[0;34m\]"
-Reset="\[\033[0m\]"
-PROMPT_COMMAND='__git_ps1 "${Blue}\w${Reset}" "\n\$ "'
+PROMPT_COMMAND='__git_ps1 "\[\e[34m\]\w\[\e[m\]" "\[\] "'
