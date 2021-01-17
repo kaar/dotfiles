f7a0c65b73b65d14c1cb21e8c97bd7f410e9ee74
Caspar Nettelbladt <caspar.n@gmail.com>
2021-01-17 21:02:55 +0100

    change prompt color to a lighter color of blue

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,4 +1,4 @@
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
-PROMPT_COMMAND='__git_ps1 "\[\e[34m\]\w\[\e[m\]" "\[\] "'
+PROMPT_COMMAND='__git_ps1 "\[\e[01;34m\]\w\[\e[m\]" "\[\] "'
