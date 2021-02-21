c561f9d8bdc94b3f50208e290f5fe9c70853f0c6
Caspar Nettelbladt <caspar.n@gmail.com>
2021-02-21 12:14:24 +0100

    check if go exists before try to install

--- .install/go.sh
+++ .install/go.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
-set -o errexit
-set -o pipefail
-set -o nounset
+
+## Go, https://github.com/golang/go
+GOVERSION=1.15.7
 
 # Make version into a comparable integer
 version() {
 	echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
 
-
-## Go, https://github.com/golang/go
-GOVERSION=1.15.7
-INSTALLED_GOVERSION=$(go version | grep -Po "(\d+\.)+\d+")
-GO_DOWNLOAD_LINK="https://golang.org/dl/go${GOVERSION}.linux-amd64.tar.gz"
-if [ $(version $GOVERSION) -gt $(version $INSTALLED_GOVERSION) ]
+if ! type go >/dev/null 2>&1 || go version | grep -Po "(\d+\.)+\d+" -lt $GOVERSION
 then
 	echo "Install Golang $GOVERSION"
-	curl -fL "${GO_DOWNLOAD_LINK}" | sudo tar -C /usr/local -xzf -
+	curl -fL "https://golang.org/dl/go${GOVERSION}.linux-amd64.tar.gz" \
+		| sudo tar -C /usr/local -xzf -
 	echo "Create symbolic link /usr/local/go/bin/go -> /usr/bin/go"
 	sudo ln -fs /usr/local/go/bin/go /usr/bin/go
 else
