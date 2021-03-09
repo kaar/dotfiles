bbe00bfafd3c9eeae2a277d2faee3ad421215c54
Caspar Nettelbladt <caspar.n@gmail.com>
2021-03-09 14:56:16 +0100

    update prompt to only show current path

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,4 +1,8 @@
+source ~/.config/bash/git-prompt.sh
+
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
-PROMPT_COMMAND='__git_ps1 "\[\e[01;34m\]\w\[\e[m\]" "\[\] "'
+
+#PS1='${PWD#"${PWD%/*/*}/"} \$ '
+PROMPT_COMMAND='__git_ps1 "\[\e[01;34m\]\W\[\e[m\]" "\[\] "'
