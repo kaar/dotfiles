0f700afc6dabc19241445080a95d85183fda556a
Caspar Nettelbladt <caspar.n@gmail.com>
2021-03-17 17:07:38 +0100

    add install-keyrate

---
+++ .install/install-keyrate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+## Keyrate, https://github.com/EricTetz/keyrate
+## Increase keyboard repeat above OS limit
+## Usage
+## keyrate <delay> <repeat>
+## keyrate 200 15   - good setting
+## keyrate          - reset
+
+BIN=$HOME/.local/bin
+
+if [ -z "$(which keyrate)" ]; then
+  curl -sfL "https://github.com/EricTetz/keyrate/releases/download/v1.0/keyrate.exe" \
+    -o $BIN/keyrate
+  chmod +x $BIN/keyrate
+else
+  echo "keyrate is already installed"
+fi
