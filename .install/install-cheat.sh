96078c276669f8ec83bcae876c2dcb511c81a94c
Caspar Nettelbladt <caspar.n@gmail.com>
2021-04-24 12:08:28 +0200

    fix install-cheat.sh

--- .install/install-cheat.sh
+++ .install/install-cheat.sh
@@ -3,10 +3,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-BIN=~/.bin
-mkdir -p $BIN
-
 ## cheat.sh, https://github.com/chubin/cheat.sh
-curl --silent https://cht.sh/:cht.sh > $BIN/cht.sh
-chmod +x $BIN/cht.sh
-curl --silent https://cheat.sh/:bash_completion > ~/.config/cht.sh/completion.bash
+curl --silent https://cht.sh/:cht.sh > "${BIN}/cht.sh"
+chmod +x "${BIN}/cht.sh"
+curl --silent https://cheat.sh/:bash_completion > ${HOME}/.config/bash_completion.d/cht.sh
