312e4dba0bd5cc0f186b94c885ed2904b02c21e3
Caspar Nettelbladt <caspar.n@gmail.com>
2021-06-14 18:09:45 +0200

    add aws/get-session-token script

---
+++ .scripts/aws/get-session-token.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+response=$(aws sts get-session-token --serial-number $AWS_MDA_SERIAL --token-code $1)
+
+export AWS_ACCESS_KEY_ID="$(echo $response | jq -r ".Credentials.AccessKeyId")"
+export AWS_SECRET_ACCESS_KEY="$(echo $response | jq -r ".Credentials.SecretAccessKey")"
+export AWS_SESSION_TOKEN="$(echo $response | jq -r ".Credentials.SessionToken")"
