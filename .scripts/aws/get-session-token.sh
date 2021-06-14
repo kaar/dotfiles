a5b47a88f8f9cb8b0ab05ad6c4b246c1c31310e3
Caspar Nettelbladt <caspar.n@gmail.com>
2021-06-14 18:11:15 +0200

    update get-session-token script

--- .scripts/aws/get-session-token.sh
+++ .scripts/aws/get-session-token.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-response=$(aws sts get-session-token --serial-number $AWS_MDA_SERIAL --token-code $1)
+response=$(aws sts get-session-token --serial-number $AWS_MFA_SERIAL --token-code $1)
 
 export AWS_ACCESS_KEY_ID="$(echo $response | jq -r ".Credentials.AccessKeyId")"
 export AWS_SECRET_ACCESS_KEY="$(echo $response | jq -r ".Credentials.SecretAccessKey")"
