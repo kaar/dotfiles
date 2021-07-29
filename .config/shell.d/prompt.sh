a1f75be3263f54122f7ca75ce782e19f98c70614
Caspar Nettelbladt <caspar.n@gmail.com>
2021-07-29 10:05:18 +0200

    remove separated shell files that are now part of .bashrc

--- .config/shell.d/prompt.sh
+++
@@ -1,8 +0,0 @@
-source ~/.config/bash/git-prompt.sh
-
-export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWCOLORHINTS=1
-export GIT_PS1_SHOWUNTRACKEDFILES=1
-
-#PS1='${PWD#"${PWD%/*/*}/"} \$ '
-PROMPT_COMMAND='__git_ps1 "\[\e[01;34m\]\W\[\e[m\]" "\[\] "'
