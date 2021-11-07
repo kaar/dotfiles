ed584a027ab5f83f6b97aa0179342686795614fb
Caspar Nettelbladt <caspar.n@gmail.com>
2021-11-07 16:37:59 +0100

    lvim: add copilot

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -140,11 +140,51 @@ lvim.lang.python.linters = {
 }
 
 
+-- lvim.keys.normal_mode = {
+--  -- Navigate buffers
+--  ["<Tab>"] = "",
+--  ["<S-Tab>"] = "",
+-- }
+
 -- Additional Plugins
 lvim.plugins = {
     {"sainnhe/gruvbox-material"},
     {"tpope/vim-fugitive"},
+    {"github/copilot.vim"},
 }
+-- https://github.com/LunarVim/LunarVim/issues/1856
+-- configure copilot
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+
+local cmp = require "cmp"
+
+
+-- map copilot to ctrl-e
+-- lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
+--   cmp.mapping.abort()
+--   local copilot_keys = vim.fn["copilot#Accept"]()
+--   if copilot_keys ~= "" then
+--     vim.api.nvim_feedkeys(copilot_keys, "i", true)
+--   else
+--     fallback()
+--   end
+-- end
+
+-- re-map tab to use copilot
+lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  else
+    local copilot_keys = vim.fn["copilot#Accept"]()
+    if copilot_keys ~= "" then
+      vim.api.nvim_feedkeys(copilot_keys, "i", true)
+    else
+      fallback()
+    end
+  end
+end
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
