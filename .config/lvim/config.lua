424f275de1bc5dc568945fd4bcd600b44225b6fa
Caspar Nettelbladt <caspar.n@gmail.com>
2021-11-25 12:05:24 +0100

    lvim: add custom keymapping

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -23,6 +23,19 @@ lvim.lsp.diagnostics.virtual_text = false
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
+-- Easy insertion of a trailing ; or , from insert mode
+lvim.keys.insert_mode[";;"] = "<ESC>A;<ESC>"
+lvim.keys.insert_mode["::"] = "<ESC>A:<ESC>"
+
+-- Maintain the cursor position when yanking a visual selection
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+lvim.keys.visual_mode["y"] = "myy`y"
+lvim.keys.visual_mode["Y"] = "myY`y"
+
+-- Paste replace visual selection without copying it
+lvim.keys.visual_mode["<leader>p"] = '"_dP'
+
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
