32c1d7b3357e5f5abada956c40f9786fefae83c9
Caspar Nettelbladt <caspar.n@gmail.com>
2022-12-19 10:48:02 +0100

    nvim: Delete old config

--- .config/nvim/lua/kaar/init.lua
+++
@@ -1,5 +0,0 @@
-require("kaar.options")
-require("kaar.plugins")
-
--- local options = require "kaar.options"
--- options.load_default_options()
--- .config/nvim/lua/kaar/plugins.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'sainnhe/gruvbox-material'
-  use 'christoomey/vim-tmux-navigator'
-end)
