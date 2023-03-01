d2d1d8647bc6a1047c4fd7fbcf88e15a7eea84ad
Caspar Nettelbladt <caspar.n@gmail.com>
2023-03-01 15:10:53 +0100

    install: add argocd.sh

---
+++ .install/argocd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# https://argo-cd.readthedocs.io/en/stable/cli_installation/
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
