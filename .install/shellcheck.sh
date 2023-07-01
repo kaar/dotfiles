390b4b9eae00b9805f39a15b751cdcc82e98dc6d
Caspar Nettelbladt <caspar.n@gmail.com>
2023-07-01 15:06:03 +0200

    install: shellcheck, clean up after install

--- .install/shellcheck.sh
+++ .install/shellcheck.sh
@@ -5,5 +5,7 @@ wget -qO- \
   "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" \
   | tar -xJv
 cp "shellcheck-${scversion}/shellcheck" "${HOME}/.local/bin/"
+rm -rf "shellcheck-${scversion}"
 shellcheck --version
+
 
