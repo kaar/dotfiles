40527deb53969a8e7184f87092f84bb8e4435268
Caspar Nettelbladt <caspar.n@gmail.com>
2024-05-25 06:29:54 +0200

    nvim: format

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim'},
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
   -- Autoinstall languages that are not installed
   auto_install = true,
   highlight = { enable = true },
