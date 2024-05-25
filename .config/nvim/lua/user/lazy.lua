28d5d63f909dff7d9c0bde455aaa2035554493f1
Caspar Nettelbladt <caspar.n@gmail.com>
2024-05-25 06:40:31 +0200

    nvim: Add Lualine

--- .config/nvim/lua/user/lazy.lua
+++ .config/nvim/lua/user/lazy.lua
@@ -105,19 +105,19 @@ require('lazy').setup({
     'github/copilot.vim',
   },
 
-  -- {
-  --   -- Set lualine as statusline
-  --   'nvim-lualine/lualine.nvim',
-  --   -- See `:help lualine.txt`
-  --   opts = {
-  --     options = {
-  --       icons_enabled = false,
-  --       theme = 'gruvbox',
-  --       component_separators = '|',
-  --       section_separators = '',
-  --     },
-  --   },
-  --   dependencies = { 'nvim-tree/nvim-web-devicons' }
-  -- },
+  -- Lualine
+  -- See :help lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'gruvbox',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
 
 }, {})
