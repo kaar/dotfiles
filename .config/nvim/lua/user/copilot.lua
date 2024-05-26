f0f083d298f36cafc213949b33266fc47cdf08b2
Caspar Nettelbladt <caspar.n@gmail.com>
2024-05-26 19:41:51 +0200

    nvim: fix tab mapping copilot

--- .config/nvim/lua/user/copilot.lua
+++ .config/nvim/lua/user/copilot.lua
@@ -1,6 +1,5 @@
-vim.g.copilot_assume_mapped = true
-vim.g.copilot_tab_fallback = ""
 vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = false
 
 vim.keymap.set('i', '<C-E>', 'copilot#Accept("<CR>")', {
   expr = true,
