e3fd73bf2d252055c3f5e7608d87e6c3b6bd3e10
Caspar Nettelbladt <caspar.n@gmail.com>
2024-05-26 13:11:20 +0200

    nvim: update telescope keymaps

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -16,10 +16,12 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-keymap('n', '<leader>f', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-keymap('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-keymap('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-keymap('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+local builtin = require('telescope.builtin')
+
+keymap('n', '<leader>f', builtin.find_files, { desc = '[S]earch [F]iles' })
+keymap('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+keymap('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+keymap('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 keymap('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -27,5 +29,6 @@ keymap('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
-keymap('n', '<leader>st', require('telescope.builtin').live_grep, { desc = '[S]earch [T]ext' })
-keymap('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+keymap('n', '<leader>st', builtin.live_grep, { desc = '[S]earch [T]ext' })
+keymap('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+keymap('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
