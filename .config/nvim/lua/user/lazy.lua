036f8832211ac88ccd8aa10e833e81dd3dcd7ed6
Caspar Nettelbladt <caspar.n@gmail.com>
2024-06-09 18:59:19 +0200

    nvim: mondularize lazy plugin setup
    This commit restructures the Lazy plugin setup to load all plugins
    from the `./user/plugins/*` directory.

--- .config/nvim/lua/user/lazy.lua
+++ .config/nvim/lua/user/lazy.lua
@@ -2,134 +2,24 @@
 -- See `:help lazy.nvim.txt`
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	"christoomey/vim-tmux-navigator",
-	-- Nvim Tree: https://github.com/nvim-tree/nvim-tree.lua
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
-
-	-- OIL, File explorer
-	-- https://github.com/stevearc/oil.nvim
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("oil").setup {
-				columns = {
-					"icon",
-				},
-				view_options = {
-					show_hidden = true,
-				},
-				default_file_explorer = true,
-			}
-		end,
-	},
-
-	-- Colorscheme
-	{
-		"sainnhe/gruvbox-material",
-		config = function()
-			vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
-
-	-- Detect tabstop and shiftwidth automatically
-	"tpope/vim-sleuth",
-
-	-- "gc" to comment visual regions/lines
-	{ "numToStr/Comment.nvim", opts = {} },
-
-	-- Fuzzy Finder (files, lsp, etc)
-	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-	-- Only load if `make` is available. Make sure you have the system
-	-- requirements installed.
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		-- NOTE: If you are having trouble with this installation,
-		--       refer to the README for telescope-fzf-native for more instructions.
-		build = "make",
-		cond = function()
-			return vim.fn.executable("make") == 1
-		end,
-	},
-
-	-- Treesitter, highlight, edit, and navigate code
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		build = ":TSUpdate",
-	},
-
-	-- Autopairs
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
-
-	-- LSP
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
-			{ "williamboman/mason.nvim", config = true },
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
-
-			-- Useful status updates for LSP
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			-- { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
-
-			-- Additional lua configuration, makes nvim stuff amazing!
-			"folke/neodev.nvim",
-		},
-	},
-
-	-- https://github.com/github/copilot.vim
-	{ "github/copilot.vim" },
-
-	-- Lualine
-	-- See :help lualine
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = {
-			options = {
-				icons_enabled = false,
-				theme = "gruvbox",
-				component_separators = "|",
-				section_separators = "",
-			},
-		},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-
-	-- https://github.com/stevearc/conform.nvim
-	{ "stevearc/conform.nvim" },
-}, {})
+-- This loads all the plugins directly from ./user/plugins/*
+-- Took inspiration from https://github.com/tjdevries/config.nvim
+require("lazy").setup(
+  { import = "user/plugins" },
+  {
+    change_detection = {
+      notify = true,
+    }
+  }
+)
