a6ff218b5ba7cc9502bf53574bd396f331ff5166
Caspar Nettelbladt <caspar.n@gmail.com>
2025-07-12 09:58:41 +0200

    install: update golang install script

--- .install/install-golang.sh
+++ .install/install-golang.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
-## Go, https://github.com/golang/go
-GOVERSION=1.15.7
+# Go, https://github.com/golang/go
+GOVERSION=1.24.4
 
-# Make version into a comparable integer
-version() {
-	echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
-}
-
-if ! type go >/dev/null 2>&1 || go version | grep -Po "(\d+\.)+\d+" -lt $GOVERSION
-then
-	echo "Install Golang $GOVERSION"
-	curl -fL "https://golang.org/dl/go${GOVERSION}.linux-amd64.tar.gz" \
-		| sudo tar -C /usr/local -xzf -
-	echo "Create symbolic link /usr/local/go/bin/go -> /usr/bin/go"
-	sudo ln -fs /usr/local/go/bin/go /usr/bin/go
-else
-	echo "Already latest version $GOVERSION"
-fi
+echo "Install Golang $GOVERSION"
+rm -rf /usr/local/go \
+  && curl -fL "https://golang.org/dl/go${GOVERSION}.linux-amd64.tar.gz" \
+  | sudo tar -C /usr/local -xzf -
+echo "Create symbolic link /usr/local/go/bin/go -> /usr/bin/go"
+sudo ln -fs /usr/local/go/bin/go /usr/bin/go
