28a4e00b85c7ab0d88e4eb073c8929cdc5d9215a
Caspar Nettelbladt <caspar.n@gmail.com>
2025-07-18 19:09:56 +0200

    install: update golang install

--- .install/install-golang.sh
+++ .install/install-golang.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Go, https://github.com/golang/go
-GOVERSION=1.24.4
+GOVERSION=1.24.5
 
 echo "Install Golang $GOVERSION"
-rm -rf /usr/local/go \
+GOPATH=${HOME}/go
+rm -rf "$GOPATH" && mkdir -p "$GOPATH" \
   && curl -fL "https://golang.org/dl/go${GOVERSION}.linux-amd64.tar.gz" \
-  | sudo tar -C /usr/local -xzf -
-echo "Create symbolic link /usr/local/go/bin/go -> /usr/bin/go"
-sudo ln -fs /usr/local/go/bin/go /usr/bin/go
+  | tar -C "$GOPATH" -xzf -
+
+ln -sf "$GOPATH/go/bin/go" "$HOME/.local/bin/go"
